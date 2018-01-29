71f2e2ed660307a247e71494f7453d11c839c970
littlestone <littlestone>
2018-01-29 18:07:52 +0800

    feat： 新增课程标签管理 课程团集会模板静态页面

--- src/common/router.js
+++ src/common/router.js
@@ -163,6 +163,12 @@ export const getRouterData = (app) => {
     '/course/type': {
       component: dynamicWrapper(app, [], () => import('../routes/Course/CourseType')),
     },
+    '/course/tag': {
+      component: dynamicWrapper(app, [], () => import('../routes/Course/CourseTag')),
+    },
+    '/course/team-template': {
+      component: dynamicWrapper(app, [], () => import('../routes/Course/TeamTemplate')),
+    }
   };
   // Get name from ./menu.js or just set it in the router data.
   const menuData = getFlatMenuData(getMenuData());
