6864787c0a63528a95a7865578e47caf00c1f735
littlestone <littlestone>
2018-03-08 10:09:17 +0800

    fix：补充token 判断条件,

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import '@babel/polyfill';
 import 'url-polyfill';
 import dva from 'dva';
-import {notification} from "antd";
-import {parseError} from './utils/utils';
+import { notification } from 'antd';
+import { parseError } from './utils/utils';
 import createHistory from 'history/createHashHistory';
 // user BrowserHistory
 // import createHistory from 'history/createBrowserHistory';
@@ -18,23 +18,28 @@ const app = dva({
   history: createHistory(),
   onError(e) {
     e.preventDefault();
-    if( e.message === "rest136|会话已过期" || e.message.toLocaleLowerCase() === 'token is expired' || e.message === 'TokenExpiredError: jwt expired') {
+    if (
+      e.message === 'rest136|会话已过期' ||
+      e.message.toLocaleLowerCase() === 'token is expired' ||
+      e.message.toLocaleLowerCase() === 'token expired' ||
+      e.message === 'TokenExpiredError: jwt expired'
+    ) {
       notification['error']({
         message: '错误警告',
-        description: "会话已过期, 请重新登录",
+        description: '会话已过期, 请重新登录',
         duration: 2
       });
-      setTimeout(function () {
+      setTimeout(function() {
         localStorage.setItem('authority', 'guest');
         localStorage.removeItem('token');
         localStorage.removeItem('uid');
         localStorage.removeItem('exp');
         window.location.reload();
-      },2500)
-    }else {
+      }, 2500);
+    } else {
       notification['error']({
         message: '错误警告',
-        description: parseError(e.message),
+        description: parseError(e.message)
       });
     }
   },
@@ -53,7 +58,6 @@ app.router(require('./router').default);
 // 5. Start
 app.start('#root');
 
-
 FastClick.attach(document.body);
 
-export default app._store;  // eslint-disable-line
+export default app._store; // eslint-disable-line
