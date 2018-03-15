352f9dfe94bb0a0c984f282a5c326689d82f5b80
littlestone <littlestone>
2018-03-15 18:09:33 +0800

    fix：升级依赖, 完善添加商品页面

--- src/routes/Mall/GoodsAdd.js
+++ src/routes/Mall/GoodsAdd.js
@@ -1,54 +1,103 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
-import { Card, Input, Button, Form, Upload, Icon, Radio, Modal } from 'antd';
+import {
+  Row,
+  Col,
+  Card,
+  Input,
+  Button,
+  Form,
+  Upload,
+  Icon,
+  Radio,
+  Modal,
+  DatePicker,
+  InputNumber,
+  Cascader,
+  Select,
+  Checkbox
+} from 'antd';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
-import { thumbnailPath, rootUrl } from "../../utils/constant";
-import {message} from "antd/lib/index";
+import { thumbnailPath, rootUrl } from '../../utils/constant';
+import options from '../../utils/sell-address-options';
+import styles from './index.less';
+import {doExchange} from '../../utils/utils';
+import colors from '../../static/colors';
+// 引入编辑器以及编辑器样式
+import BraftEditor from 'braft-editor';
+import 'braft-editor/dist/braft.css';
+
 const FormItem = Form.Item;
 const RadioGroup = Radio.Group;
+const Option = Select.Option;
+const CheckboxGroup = Checkbox.Group;
+
+const sizeOptions = [
+  { label: 'S', value: 'S' },
+  { label: 'M', value: 'M' },
+  { label: 'XL', value: 'XL' },
+  { label: 'XXL', value: 'XXL' },
+  { label: 'XXXL', value: 'XXXL' }
+];
+let uuid = 0;
+
 @connect(({ mall, loading }) => ({
   mall,
   loading: loading.models.mall
 }))
 @Form.create()
 class GoodsAdd extends Component {
-
   state = {
     previewVisible: false,
     previewImage: '',
-    fileList:[]
+    fileList: []
   };
 
   hasErrors(fieldsError) {
     return Object.keys(fieldsError).some(field => fieldsError[field]);
   }
 
-  handleCancel = () => this.setState({ previewVisible: false })
+  /**
+   * 取消预览
+   */
+  handleCancel = () => this.setState({ previewVisible: false });
 
-  handlePreview = (file) => {
+  /**
+   * 上传图片的预览
+   * @param file
+   */
+  handlePreview = file => {
     this.setState({
       previewImage: rootUrl + thumbnailPath + file.response.filename,
-      previewVisible: true,
+      previewVisible: true
     });
-  }
+  };
 
+  /**
+   * 提交商品新建
+   * @param e
+   */
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
-        console.log("添加商品参数: ",values)
+        console.log('添加商品参数: ', values);
       }
-    })
+    });
   };
 
-  handleChange = (info) => {
+  /**
+   * 商品图片上传
+   * @param info
+   */
+  handleChange = info => {
     let fileList = info.fileList;
     // 1. Limit the number of uploaded files
     //    Only to show two recent uploaded files, and old ones will be replaced by the new
     // fileList = fileList.slice(-2);
 
     // 2. read from response and show file link
-    fileList = fileList.map((file) => {
+    fileList = fileList.map(file => {
       // console.log("file==>", file);
       if (file.response) {
         // Component will show file.url as link
@@ -59,18 +108,64 @@ class GoodsAdd extends Component {
       }
       return file;
     });
-    console.log("fileList map: ", fileList);
+    console.log('fileList map: ', fileList);
     this.setState({ fileList });
   };
 
+  /**
+   * 正文图片上传
+   * @param param
+   */
+  uploadFn = param => {
+    // console.log("param==>", param);
+    const xhr = new XMLHttpRequest();
+    const fd = new FormData();
+    const mediaLibrary = this.editorInstance.getMediaLibraryInstance();
+
+    const successFn = response => {
+      // console.log("图片上传成功:", JSON.parse(xhr.responseText));
+      const fileName = JSON.parse(xhr.responseText).filename;
+      const imgUrl = 'https://api.yichui.net/api/young/post/download/image/origin/' + fileName;
+      param.success({ url: imgUrl });
+    };
+
+    const progressFn = event => {
+      param.progress(event.loaded / event.total * 100);
+    };
+
+    const errorFn = response => {
+      param.error({
+        msg: 'unable to upload.'
+      });
+    };
+
+    xhr.upload.addEventListener('progress', progressFn, false);
+    xhr.addEventListener('load', successFn, false);
+    xhr.addEventListener('error', errorFn, false);
+    xhr.addEventListener('abort', errorFn, false);
+
+    fd.append('file', param.file);
+    xhr.open('POST', 'https://api.yichui.net/api/young/post/upload/image', true);
+    xhr.send(fd);
+  };
+
+  permuteAndCombine = () => {
+    const arr = [
+      ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+      ['Red', 'Blue', 'Green']
+    ];
+    console.log(doExchange(arr));
+  };
+
   render() {
-    const {fileList, previewVisible, previewImage} = this.state;
-    const {
-      getFieldDecorator,
-      getFieldsError,
-      getFieldError,
-      isFieldTouched
-    } = this.props.form;
+    const { fileList, previewVisible, previewImage } = this.state;
+    const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched, getFieldValue } = this.props.form;
+    const editorProps = {
+      height: 200,
+      tabIndents: 2,
+      contentFormat: 'html',
+      initialContent: '<p>Hello World!</p>'
+    };
     const uploadButton = (
       <div>
         <Icon type="plus" />
@@ -82,7 +177,7 @@ class GoodsAdd extends Component {
       name: 'file',
       action: 'https://api.yichui.net/api/young/post/upload/image',
       onChange: this.handleChange,
-      multiple: true,
+      multiple: true
     };
 
     const formItemLayout = {
@@ -92,7 +187,7 @@ class GoodsAdd extends Component {
       },
       wrapperCol: {
         xs: { span: 24 },
-        sm: { span: 12 },
+        sm: { span: 20 },
         md: { span: 10 }
       }
     };
@@ -106,15 +201,15 @@ class GoodsAdd extends Component {
     const breadcrumbList = [
       {
         title: '首页',
-        href: '/',
+        href: '/'
       },
       {
         title: '商品管理',
-        href: '/mall/goods-list',
+        href: '/mall/goods-list'
       },
       {
         title: '添加商品',
-        href: '/mall/goods-add',
+        href: '/mall/goods-add'
       }
     ];
 
@@ -123,11 +218,8 @@ class GoodsAdd extends Component {
     return (
       <PageHeaderLayout breadcrumbList={breadcrumbList}>
         <Card bordered={false}>
-          <Form
-            onSubmit={this.handleSubmit}
-            hideRequiredMark
-            style={{ marginTop: 8 }}
-          >
+          <Button onClick={() => this.permuteAndCombine()}>测试排列组合</Button>
+          <Form onSubmit={this.handleSubmit} hideRequiredMark style={{ marginTop: 8 }}>
             <FormItem
               {...formItemLayout}
               validateStatus={badgeNameError ? 'error' : ''}
@@ -135,13 +227,13 @@ class GoodsAdd extends Component {
               label="商品类型"
             >
               {getFieldDecorator('type', {
-                rules: [
-                  {
-                    required: true,
-                    message: '请输入商品类型'
-                  }
-                ]
-              })(<Input placeholder="商品类型" />)}
+                initialValue: 0
+              })(
+                <Select>
+                  <Option value={0}>普通商品</Option>
+                  <Option value={1}>虚拟商品</Option>
+                </Select>
+              )}
             </FormItem>
             <FormItem
               {...formItemLayout}
@@ -152,7 +244,7 @@ class GoodsAdd extends Component {
               {getFieldDecorator('name', {
                 rules: [
                   {
-                    required: true,
+                    required: false,
                     message: '请输入商品名称'
                   }
                 ]
@@ -167,140 +259,89 @@ class GoodsAdd extends Component {
               {getFieldDecorator('sku', {
                 rules: [
                   {
-                    required: true,
+                    required: false,
                     message: '请输入商品编号'
                   }
                 ]
               })(<Input placeholder="商品编号" />)}
             </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label="商品图片"
-            >
+            <FormItem {...formItemLayout} label="商品图片">
               {getFieldDecorator('imgs', {
-                rules: [{ required: true }]
+                rules: [{ required: false }]
               })(
-                <Upload
-                  {...propsObj}
-                  fileList={fileList}
-                  listType="picture-card"
-                  onPreview={this.handlePreview}
-                >
+                <Upload {...propsObj} fileList={fileList} listType="picture-card" onPreview={this.handlePreview}>
                   {fileList.length >= 3 ? null : uploadButton}
                 </Upload>
               )}
             </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label="商品规格"
-            >
+            <FormItem {...formItemLayout} label="商品规格">
               {getFieldDecorator('skuSize')(
                 <RadioGroup>
                   <Radio value="a">没有规格</Radio>
-                  <Radio value="b">有规格</Radio>
+                  <Radio value="b">有规格</Radio> {/*  ['XXL-red','XXL-blue']  */}
                 </RadioGroup>
               )}
             </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label="出售区域"
-            >
-              {getFieldDecorator('radio-group')(
-                <RadioGroup>
-                  <Radio value="a">没有规格</Radio>
-                  <Radio value="b">有规格</Radio>
-                </RadioGroup>
+            <FormItem {...formItemLayout} label="颜色">
+              {getFieldDecorator('color')(
+                <CheckboxGroup options={colors} />
               )}
             </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label="库存"
-            >
-              {getFieldDecorator('stock')(
-                <Input />
-              )}
+
+            <FormItem {...formItemLayout} label="尺寸">
+              {getFieldDecorator('size')(<CheckboxGroup options={sizeOptions} />)}
             </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label="原价"
-            >
-              {getFieldDecorator('originalPrice')(
-                <Input />
-              )}
+
+            <FormItem {...formItemLayout} label="出售区域">
+              {getFieldDecorator('address')(<Cascader options={options} changeOnSelect={true} />)}
             </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label="折扣"
-            >
-              {getFieldDecorator('discount')(
-                <Input />
-              )}
+            <FormItem {...formItemLayout} label="库存">
+              {getFieldDecorator('stock')(<InputNumber min={0} max={10000000} style={{ width: '100%' }} />)}
             </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label="折后价"
-            >
-              {getFieldDecorator('price')(
-                <Input />
-              )}
+            <FormItem {...formItemLayout} label="原价">
+              {getFieldDecorator('originalPrice')(<InputNumber min={0} max={10000000} style={{ width: '100%' }} />)}
             </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label="产品描述"
-            >
-              {getFieldDecorator('description')(
-                <Input />
-              )}
+            <FormItem {...formItemLayout} label="折扣">
+              {getFieldDecorator('discount')(<InputNumber min={0} max={10000000} style={{ width: '100%' }} />)}
             </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label="邮费"
-            >
-              {getFieldDecorator('postPrice')(
-                <Input />
-              )}
+            <FormItem {...formItemLayout} label="折后价">
+              {getFieldDecorator('price')(<InputNumber min={0} max={10000000} style={{ width: '100%' }} />)}
             </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label="赠品"
-            >
-              {getFieldDecorator('goodsJson')(
-                <Input />
-              )}
+            <FormItem label="产品描述">
+              <div className={styles.editorWrap}>
+                {getFieldDecorator('description')(
+                  <BraftEditor
+                    {...editorProps}
+                    ref={instance => (this.editorInstance = instance)}
+                    media={{
+                      image: true,
+                      uploadFn: param => this.uploadFn(param)
+                    }}
+                  />
+                )}
+              </div>
             </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label="下架时间"
-            >
-              {getFieldDecorator('upTime')(
-                <Input />
-              )}
+            <FormItem {...formItemLayout} label="邮费">
+              {getFieldDecorator('postPrice')(<InputNumber min={0} max={10000000} style={{ width: '100%' }} />)}
             </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label="过期时间"
-            >
-              {getFieldDecorator('downTime')(
-                <Input />
-              )}
+            <FormItem {...formItemLayout} label="赠品">
+              {getFieldDecorator('goodsJson')(<Input />)}
             </FormItem>
-            <FormItem
-              {...formItemLayout}
-              label="产品描述"
-            >
-              {getFieldDecorator('description')(
-                <Input />
-              )}
+            <FormItem {...formItemLayout} label="上架时间">
+              {getFieldDecorator('upTime')(<DatePicker style={{ width: '100%' }} />)}
+              <span>不需要显示的商品填写 2999-12-30</span>
+            </FormItem>
+            <FormItem {...formItemLayout} label="下架时间">
+              {getFieldDecorator('downTime')(<DatePicker style={{ width: '100%' }} />)}
             </FormItem>
             <FormItem {...submitFormLayout} style={{ marginTop: 32 }}>
               <Button
                 type="primary"
                 htmlType="submit"
-                disabled={this.hasErrors(getFieldsError())}
+                // disabled={this.hasErrors(getFieldsError())}
               >
                 提交
               </Button>
-              {/*<Button style={{ marginLeft: 8 }}>保存</Button>*/}
             </FormItem>
           </Form>
         </Card>
@@ -308,7 +349,7 @@ class GoodsAdd extends Component {
           <img alt="example" style={{ width: '100%' }} src={previewImage} />
         </Modal>
       </PageHeaderLayout>
-    )
+    );
   }
 }
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -254,3 +254,40 @@ export const handleType = (type) => {
   };
   return typeObj[type];
 };
+
+/**
+ * 商品规格的排列组合
+ * */
+export const doExchange = (arr) => {
+  let len = arr.length;
+  // 当数组大于等于2个的时候
+  if(len >= 2){
+    // 第一个数组的长度
+    let len1 = arr[0].length;
+    // 第二个数组的长度
+    let len2 = arr[1].length;
+    // 2个数组产生的组合数
+    let lenBoth = len1 * len2;
+    //  申明一个新数组,做数据暂存
+    let items = new Array(lenBoth);
+    // 申明新数组的索引
+    let index = 0;
+    // 2层嵌套循环,将组合放到新数组中
+    for(let i=0; i<len1; i++){
+      for(let j=0; j<len2; j++){
+        items[index] = arr[0][i] +"-"+ arr[1][j];
+        index++;
+      }
+    }
+    // 将新组合的数组并到原数组中
+    let newArr = new Array(len -1);
+    for(let i=2;i<arr.length;i++){
+      newArr[i-1] = arr[i];
+    }
+    newArr[0] = items;
+    // 执行回调
+    return doExchange(newArr);
+  }else{
+    return arr[0];
+  }
+};
