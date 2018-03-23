f3441d229206830558d6550de48045ca151039e7
littlestone <littlestone>
2018-03-23 15:53:46 +0800

    fix: 添加商品 bug 修改

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,13 +1,13 @@
-/*export const gqlNetworkInterface = "https://api.yichui.net/api/young/graphql";
+export const gqlNetworkInterface = "https://api.yichui.net/api/young/graphql";
 export const wsUrl = "wss://api.yichui.net/api/young/ws";
 export const rootUrl = "https://api.yichui.net";
 export const thumbnailPath = "/api/young/post/download/image/thumbnail/";
 export const originPath = "/api/young/post/download/image/origin/";
-export const uploadPath = "https://api.yichui.net/api/young/post/upload/image";*/
+export const uploadPath = "https://api.yichui.net/api/young/post/upload/image";
 
-export const gqlNetworkInterface = "https://api.bldsnapp.com/api/young/graphql";
+/*export const gqlNetworkInterface = "https://api.bldsnapp.com/api/young/graphql";
 export const wsUrl = "wss://api.bldsnapp.com/api/young/ws";
 export const rootUrl = "https://api.bldsnapp.com";
 export const thumbnailPath = "/api/young/post/download/image/thumbnail/";
 export const originPath = "/api/young/post/download/image/origin/";
-export const uploadPath = "https://api.bldsnapp.com/api/young/post/upload/image";
+export const uploadPath = "https://api.bldsnapp.com/api/young/post/upload/image";*/
