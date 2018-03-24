6002521aea7eab1a9b74f5c3a9f948d51b2ca5da
littlestone <littlestone>
2018-03-24 18:49:06 +0800

    fix: 修改部署脚本

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,4 @@
+// 本地服务器
 export const gqlNetworkInterface = "https://api.yichui.net/api/young/graphql";
 export const wsUrl = "wss://api.yichui.net/api/young/ws";
 export const rootUrl = "https://api.yichui.net";
@@ -5,6 +6,7 @@ export const thumbnailPath = "/api/young/post/download/image/thumbnail/";
 export const originPath = "/api/young/post/download/image/origin/";
 export const uploadPath = "https://api.yichui.net/api/young/post/upload/image";
 
+// 在线服务器
 /*export const gqlNetworkInterface = "https://api.bldsnapp.com/api/young/graphql";
 export const wsUrl = "wss://api.bldsnapp.com/api/young/ws";
 export const rootUrl = "https://api.bldsnapp.com";
