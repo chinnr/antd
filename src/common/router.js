8bbb5a0a168f0673294eabfb34f3d9490a043328
chb <SpawN>
2018-04-02 17:49:56 +0000

    fix:添加商品修改组件的路由

--- src/common/router.js
+++ src/common/router.js
@@ -219,6 +219,9 @@ export const getRouterData = (app) => {
     '/mall/goods-add': {
       component: dynamicWrapper(app, ['mall'], () => import('../routes/Mall/GoodsAdd')),
     },
+    '/mall/goods-edit': {
+      component: dynamicWrapper(app, ['mall'], () => import('../routes/Mall/GoodsEdit')),
+    },
     '/mall/type-edit': {
       component: dynamicWrapper(app, ['mall'], () => import('../routes/Mall/GoodsTypeAdd')),
     },
