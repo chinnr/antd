d398ec84e7e16c13c87ed06b996d7b665151f191
chb <SpawN>
2018-05-19 17:00:09 +0800

    fix:修改到没有检查团长电话的版本

--- src/routes/Team/NewTeam.js
+++ src/routes/Team/NewTeam.js
@@ -1,5 +1,5 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'dva';
+import React, {PureComponent} from 'react';
+import {connect} from 'dva';
 import {
   Form,
   Input,
@@ -15,11 +15,11 @@ import {
   Col
 } from 'antd';
 import moment from 'moment';
-import { Map, Marker } from 'react-amap';
+import {Map, Marker} from 'react-amap';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
 import styles from './team.less';
 import options from '../../utils/cascader-address-options';
-import { successNotification } from '../../utils/utils';
+import {successNotification} from '../../utils/utils';
 
 const pluginProps = {
   enableHighAccuracy: true,
@@ -28,12 +28,12 @@ const pluginProps = {
 };
 
 const FormItem = Form.Item;
-const { Option } = Select;
-const { RangePicker } = DatePicker;
-const { TextArea } = Input;
+const {Option} = Select;
+const {RangePicker} = DatePicker;
+const {TextArea} = Input;
 const RadioGroup = Radio.Group;
 
-@connect(({ team }) => ({ team }))
+@connect(({team}) => ({team}))
 @Form.create()
 export default class NewTeam extends PureComponent {
   constructor() {
@@ -41,7 +41,7 @@ export default class NewTeam extends PureComponent {
     this.state = {
       modalVisible: false,
       visible: true,
-      position: { longitude: 108.291275, latitude: 22.869617 },
+      position: {longitude: 108.291275, latitude: 22.869617},
       clickable: true,
       draggable: true,
       addr: '',
@@ -72,7 +72,7 @@ export default class NewTeam extends PureComponent {
         .then(res => {
           console.log('res locationInfo: ', res);
           // 重新设置输入框地址
-          this.props.form.setFieldsValue({ address: res.locationInfo.format });
+          this.props.form.setFieldsValue({address: res.locationInfo.format});
           this.isDragMap = true;
           this.dragLocationInfo = res.locationInfo;
         })
@@ -85,11 +85,11 @@ export default class NewTeam extends PureComponent {
     this.props
       .dispatch({
         type: 'team/addrInfo',
-        payload: { input: this.state.addr }
+        payload: {input: this.state.addr}
       })
       .then(res => {
         console.log('view res: ', res);
-        const { addressInfo } = res;
+        const {addressInfo} = res;
         const _position = {
           longitude: addressInfo.longitude,
           latitude: addressInfo.latitude
@@ -126,11 +126,11 @@ export default class NewTeam extends PureComponent {
 
   // 处理地址
   handleAddrChange = e => {
-    this.setState({ addr: e.target.value }, () => {
+    this.setState({addr: e.target.value}, () => {
       if (this.state.addr.length > 0) {
-        this.setState({ clickMap: true });
+        this.setState({clickMap: true});
       } else {
-        this.setState({ clickMap: false });
+        this.setState({clickMap: false});
       }
     });
   };
@@ -142,7 +142,7 @@ export default class NewTeam extends PureComponent {
     this.props
       .dispatch({
         type: 'team/addrInfo',
-        payload: { input: e.target.value }
+        payload: {input: e.target.value}
       })
       .then(res => {
         const {addressInfo} = res;
@@ -228,7 +228,7 @@ export default class NewTeam extends PureComponent {
               }
             })
             .then(() => {
-              successNotification('新建团队成功!', function() {
+              successNotification('新建团队成功!', function () {
                 return false;
               });
             })
@@ -238,8 +238,8 @@ export default class NewTeam extends PureComponent {
     } else {
       console.log('你没有拖动标记, 将采用原来的标记');
       this.props.form.validateFieldsAndScroll((err, values) => {
-        if(err) {
-          console.log("发生错误:", err )
+        if (err) {
+          console.log("发生错误:", err)
         }
         if (!err) {
           console.log('values: ', values);
@@ -309,7 +309,7 @@ export default class NewTeam extends PureComponent {
               }
             }).then(() => {
             console.log("新建团队成功")
-            successNotification('新建团队成功!', function() {
+            successNotification('新建团队成功!', function () {
               return false;
             });
           })
@@ -320,23 +320,23 @@ export default class NewTeam extends PureComponent {
     }
   };
 
-  checkPhone = ()=>{
+  checkPhone = () => {
     console.log("check");
-    const{form} = this.props;
+    const {form} = this.props;
     const phone = form.getFieldValue("phone");
     this.props
       .dispatch({
         type: 'team/queryPhone',
-        payload:{
+        payload: {
           phone
         }
-      }).then(res=>{
-        console.log(res);
-        form.setFieldsValue({
-          userName:res.base.profile.realName,
-          userNumber:res.number
-        })
-    }).catch(err=>{
+      }).then(res => {
+      console.log(res);
+      form.setFieldsValue({
+        userName: res.base.profile.realName,
+        userNumber: res.number
+      })
+    }).catch(err => {
       alert(err)
     })
   };
@@ -352,8 +352,8 @@ export default class NewTeam extends PureComponent {
   }
 
   render() {
-    const { clickMap, showSpin } = this.state;
-    const { submitting } = this.props;
+    const {clickMap, showSpin} = this.state;
+    const {submitting} = this.props;
     const {
       getFieldDecorator,
       getFieldsError,
@@ -375,20 +375,20 @@ export default class NewTeam extends PureComponent {
 
     const formItemLayout = {
       labelCol: {
-        xs: { span: 24 },
-        sm: { span: 7 }
+        xs: {span: 24},
+        sm: {span: 7}
       },
       wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 12 },
-        md: { span: 10 }
+        xs: {span: 24},
+        sm: {span: 12},
+        md: {span: 10}
       }
     };
 
     const submitFormLayout = {
       wrapperCol: {
-        xs: { span: 24, offset: 0 },
-        sm: { span: 10, offset: 7 }
+        xs: {span: 24, offset: 0},
+        sm: {span: 10, offset: 7}
       }
     };
 
@@ -398,7 +398,7 @@ export default class NewTeam extends PureComponent {
           <Form
             onSubmit={this.handleSubmit}
             hideRequiredMark
-            style={{ marginTop: 8 }}
+            style={{marginTop: 8}}
           >
             <FormItem
               {...formItemLayout}
@@ -411,7 +411,7 @@ export default class NewTeam extends PureComponent {
                     message: '请输入团名称'
                   }
                 ]
-              })(<Input placeholder="团名称" />)}
+              })(<Input placeholder="团名称"/>)}
             </FormItem>
 
             <FormItem
@@ -443,7 +443,7 @@ export default class NewTeam extends PureComponent {
                 initialValue: moment(new Date(), 'YYYY-MM-DD')
               })(
                 <DatePicker
-                  style={{ width: '100%' }}
+                  style={{width: '100%'}}
                   placeholder={'成立时间'}
                 />
               )}
@@ -479,7 +479,7 @@ export default class NewTeam extends PureComponent {
                   }
                 ],
                 initialValue: ['全国']
-              })(<Cascader options={options} changeOnSelect={true} />)}
+              })(<Cascader options={options} changeOnSelect={true}/>)}
             </FormItem>
             <FormItem
               {...formItemLayout}
@@ -500,7 +500,7 @@ export default class NewTeam extends PureComponent {
                 />
               )}
               {showSpin &&
-                <Spin tip="正在转换地址..."/>
+              <Spin tip="正在转换地址..."/>
               }
               <Button
                 disabled={!clickMap}
@@ -521,101 +521,101 @@ export default class NewTeam extends PureComponent {
                     message: '请输入所属公司'
                   }
                 ]
-              })(<Input placeholder="所属公司" />)}
+              })(<Input placeholder="所属公司"/>)}
             </FormItem>
             <FormItem
-            {...formItemLayout}
-            validateStatus={usernameError ? 'error' : ''}
-            help={usernameError || ''}
-            label="团长账号"
+              {...formItemLayout}
+              validateStatus={usernameError ? 'error' : ''}
+              help={usernameError || ''}
+              label="团长账号"
             >
-            {getFieldDecorator('username', {
-            rules: [
-            {
-            required: true,
-            message: '请输入团长账号'
-            }
-            ]
-            })(<Input placeholder="团长账号" />)}
+              {getFieldDecorator('username', {
+                rules: [
+                  {
+                    required: true,
+                    message: '请输入团长账号'
+                  }
+                ]
+              })(<Input placeholder="团长账号"/>)}
             </FormItem>
             <FormItem
-            {...formItemLayout}
-            validateStatus={passwordError ? 'error' : ''}
-            help={passwordError || ''}
-            label="团长密码"
+              {...formItemLayout}
+              validateStatus={passwordError ? 'error' : ''}
+              help={passwordError || ''}
+              label="团长密码"
             >
-            {getFieldDecorator('password', {
-            rules: [
-            {
-            required: true,
-            message: '请输入团长密码'
-            }
-            ]
-            })(<Input placeholder="团长密码" />)}
+              {getFieldDecorator('password', {
+                rules: [
+                  {
+                    required: true,
+                    message: '请输入团长密码'
+                  }
+                ]
+              })(<Input placeholder="团长密码"/>)}
             </FormItem>
             <FormItem
-            {...formItemLayout}
-            validateStatus={teamNameError ? 'error' : ''}
-            help={teamNameError || ''}
-            label="团长姓名"
+              {...formItemLayout}
+              validateStatus={teamNameError ? 'error' : ''}
+              help={teamNameError || ''}
+              label="团长姓名"
             >
-            {getFieldDecorator('realName', {
-            rules: [
-            {
-            required: true,
-            message: '请输入团长姓名'
-            }
-            ]
-            })(<Input placeholder="团长姓名" />)}
+              {getFieldDecorator('realName', {
+                rules: [
+                  {
+                    required: true,
+                    message: '请输入团长姓名'
+                  }
+                ]
+              })(<Input placeholder="团长姓名"/>)}
             </FormItem>
 
 
             {/*<Row>*/}
-              {/*<Col xs={24} sm={24} md={7} lg={7} xl={7}><h1 className={styles.binding}>绑定团长</h1></Col>*/}
-              {/*<Col md={17} lg={17} xl={17}></Col>*/}
+            {/*<Col xs={24} sm={24} md={7} lg={7} xl={7}><h1 className={styles.binding}>绑定团长</h1></Col>*/}
+            {/*<Col md={17} lg={17} xl={17}></Col>*/}
             {/*</Row>*/}
 
             {/*<FormItem*/}
-              {/*{...formItemLayout}*/}
-              {/*label="团长电话"*/}
+            {/*{...formItemLayout}*/}
+            {/*label="团长电话"*/}
             {/*>*/}
-              {/*{getFieldDecorator('phone', {*/}
-                {/*rules: [*/}
-                  {/*{*/}
-                    {/*required: true,*/}
-                    {/*message: '请输入团长电话'*/}
-                  {/*}*/}
-                {/*]*/}
-              {/*})(<Input placeholder="团长电话" />)}*/}
-              {/*<Button type="primary" style={{position:'absolute',right:'-74px',top:'-8px'}} onClick={this.checkPhone}>检查</Button>*/}
+            {/*{getFieldDecorator('phone', {*/}
+            {/*rules: [*/}
+            {/*{*/}
+            {/*required: true,*/}
+            {/*message: '请输入团长电话'*/}
+            {/*}*/}
+            {/*]*/}
+            {/*})(<Input placeholder="团长电话" />)}*/}
+            {/*<Button type="primary" style={{position:'absolute',right:'-74px',top:'-8px'}} onClick={this.checkPhone}>检查</Button>*/}
             {/*</FormItem>*/}
             {/*<FormItem*/}
-              {/*{...formItemLayout}*/}
-              {/*label="团长编号"*/}
+            {/*{...formItemLayout}*/}
+            {/*label="团长编号"*/}
             {/*>*/}
-              {/*{getFieldDecorator('userNumber', {*/}
-                {/*rules: [*/}
-                  {/*{*/}
-                    {/*required: true,*/}
-                    {/*message: '请检查团长电话'*/}
-                  {/*}*/}
-                {/*]*/}
-              {/*})(<Input disabled={true} placeholder="团长编号" />)}*/}
+            {/*{getFieldDecorator('userNumber', {*/}
+            {/*rules: [*/}
+            {/*{*/}
+            {/*required: true,*/}
+            {/*message: '请检查团长电话'*/}
+            {/*}*/}
+            {/*]*/}
+            {/*})(<Input disabled={true} placeholder="团长编号" />)}*/}
             {/*</FormItem>*/}
             {/*<FormItem*/}
-              {/*{...formItemLayout}*/}
-              {/*label="团长姓名"*/}
+            {/*{...formItemLayout}*/}
+            {/*label="团长姓名"*/}
             {/*>*/}
-              {/*{getFieldDecorator('userName', {*/}
-                {/*rules: [*/}
-                  {/*{*/}
-                    {/*required: true,*/}
-                    {/*message: '请检查团长电话'*/}
-                  {/*}*/}
-                {/*]*/}
-              {/*})(<Input disabled={true} placeholder="团长姓名" />)}*/}
+            {/*{getFieldDecorator('userName', {*/}
+            {/*rules: [*/}
+            {/*{*/}
+            {/*required: true,*/}
+            {/*message: '请检查团长电话'*/}
+            {/*}*/}
+            {/*]*/}
+            {/*})(<Input disabled={true} placeholder="团长姓名" />)}*/}
             {/*</FormItem>*/}
-            <FormItem {...submitFormLayout} style={{ marginTop: 32 }}>
+            <FormItem {...submitFormLayout} style={{marginTop: 32}}>
               <Button
                 type="primary"
                 htmlType="submit"
@@ -635,7 +635,7 @@ export default class NewTeam extends PureComponent {
           onCancel={() => this.handleCancel()}
         >
           <div>
-            <div style={{ width: '100%', height: 360 }}>
+            <div style={{width: '100%', height: 360}}>
               <Map
                 amapkey={'a68fcf7d57d3cc225b948f23003b93f3'}
                 plugins={this.mapPlugins}
