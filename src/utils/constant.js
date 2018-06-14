3b33f29daeb5fa9cd788e7e4c1763a76d7f47ad7
littlestone <littlestone>
2018-06-14 09:22:30 +0800

    chore: 添加配置

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,7 +2,7 @@
 export const gqlNetworkInterface = "https://api.yichui.net/api/young/graphql";
 export const wsUrl = "wss://api.yichui.net/api/young/ws";
 export const rootUrl = "https://api.yichui.net";
-export const ssdUrl = "/download/young/mall/";
+export const ssdUrl = "/download/young/mall/";   // 导出地址
 export const thumbnailPath = "/api/young/post/download/image/thumbnail/";
 export const originPath = "/api/young/post/download/image/origin/";
 export const uploadPath = "https://api.yichui.net/api/young/post/upload/image";
@@ -11,7 +11,7 @@ export const uploadPath = "https://api.yichui.net/api/young/post/upload/image";
 // export const gqlNetworkInterface = "https://api.bldsnapp.com/api/young/graphql";
 // export const wsUrl = "wss://api.bldsnapp.com/api/young/ws";
 // export const rootUrl = "https://api.bldsnapp.com";
-// export const ssdUrl = "/download/young/young/mall/";
+// export const ssdUrl = "/download/young/young/mall/";  // 导出地址
 // export const thumbnailPath = "/api/young/post/download/image/thumbnail/";
 // export const originPath = "/api/young/post/download/image/origin/";
 // export const uploadPath = "https://api.bldsnapp.com/api/young/post/upload/image";
