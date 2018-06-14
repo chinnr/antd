0a0b64b6013466d5602c26d3c412cb215fa7731f
tudyzhb <tudyzhb@gmail.com>
2018-06-14 15:45:05 +0800

    chore: 默认指向测试服务器

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,19 +1,19 @@
 // 本地服务器
-/*export const gqlNetworkInterface = "https://api.yichui.net/api/young/graphql";
+export const gqlNetworkInterface = "https://api.yichui.net/api/young/graphql";
 export const wsUrl = "wss://api.yichui.net/api/young/ws";
 export const rootUrl = "https://api.yichui.net";
 export const ssdUrl = "/download/young/mall/";
 export const cpuUrl = "/download/young/young/";
 export const thumbnailPath = "/api/young/post/download/image/thumbnail/";
 export const originPath = "/api/young/post/download/image/origin/";
-export const uploadPath = "https://api.yichui.net/api/young/post/upload/image";*/
+export const uploadPath = "https://api.yichui.net/api/young/post/upload/image";
 
 // 在线服务器
-export const gqlNetworkInterface = "https://api.bldsnapp.com/api/young/graphql";
-export const wsUrl = "wss://api.bldsnapp.com/api/young/ws";
-export const rootUrl = "https://api.bldsnapp.com";
-export const ssdUrl = "/download/young/mall/";
-export const cpuUrl = "/download/young/young/";
-export const thumbnailPath = "/api/young/post/download/image/thumbnail/";
-export const originPath = "/api/young/post/download/image/origin/";
-export const uploadPath = "https://api.bldsnapp.com/api/young/post/upload/image";
+// export const gqlNetworkInterface = "https://api.bldsnapp.com/api/young/graphql";
+// export const wsUrl = "wss://api.bldsnapp.com/api/young/ws";
+// export const rootUrl = "https://api.bldsnapp.com";
+// export const ssdUrl = "/download/young/mall/";
+// export const cpuUrl = "/download/young/young/";
+// export const thumbnailPath = "/api/young/post/download/image/thumbnail/";
+// export const originPath = "/api/young/post/download/image/origin/";
+// export const uploadPath = "https://api.bldsnapp.com/api/young/post/upload/image";
